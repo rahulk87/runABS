07213178ab8e9bc85b1619b6987477afd63ecf26
Rodrigo Gularte Merida <gularter@mskcc.org>
2017-03-08 13:45:30 -0500

    Simpliefied version of Make_ABS_muts_Input.R

--- Make_ABS_muts_Input.R
+++ Make_ABS_muts_Input.R
@@ -1,29 +1,27 @@
 ## Run to make the mutation input for ABSOLUTE
+
 ## setwd('/Users/selenicp/Documents/Projects/LCIS/LCIS Exonseq/')
-filename    = 'muts.csv'     # You can change this to any file for where you stored the mutation summary sheet. 
 
-tab         = read.csv(filename, header=T, stringsAsFactors = F)
-sample_list =  unique(tab$TUMOR_SAMPLE)
+## You can change this to any file for where you stored the mutation summary sheet. 
+mutationsFile <- "muts.csv" 
+outRData <- "mutations.rda"
 
-## This goes through and removes the necessary columns from the mutation summary and 
-## reformat it to be the correct input for ABSOLUTE
-for (i in 1:length(sample_list)){
-  sample = sample_list[i]
-    Tumor_Sample_Barcode = as.character(tab$TUMOR_SAMPLE)
-    Hugo_Symbol          = paste(tab$ANN....GENE ,tab$ANN....HGVS_P,sep="_")
-    Tumor.Maf            = as.numeric(tab$TUMOR_MAF)
-    x                    = tab$TUMOR_DP*Tumor.Maf
-    t_alt_count          = round(x,0)
-    t_ref_count          = tab$TUMOR_DP - t_alt_count
+mutationSummary <- read.csv(mutationsFile, header = TRUE, stringsAsFactors = FALSE)
+sampleList <- unique(mutationSummary$TUMOR_SAMPLE)
 
-    for (i in nrow(tab)) {
-        dbSNP_Val_Status = c(rep("validated",i))
-    }
-    
-    Chromosome         = as.character(tab$CHROM)
-    Chromosome         = as.numeric(replace(Chromosome, Chromosome=="X", 23)) #Change chrX to chr23
-    Start_position     = as.numeric(tab$POS)
-    bind<-cbind.data.frame(Tumor_Sample_Barcode,Hugo_Symbol,t_ref_count,t_alt_count,dbSNP_Val_Status,Chromosome,Start_position)
-    bind = subset(bind, subset=bind$Tumor_Sample_Barcode == sample)
-    write.table(bind, paste(sample, 'muts.txt', sep="_"), sep="\t",row.names = F) # write separate file for each sample
-}
+## create a data.frame containing the colums required to run ABSOLUTE
+## columns are in the same order as the `cbind.data.frame` previously used
+## Tumor_Sample_Barcode, Hugo_Symbol, t_ref_count, t_alt_count, dbSNP_Val_Status, Chromosome, Start_position
+absIn <- data.frame(Tumor_Sample_Barcode = as.character(mutationSummary$TUMOR_SAMPLE),
+                    Hugo_Symbol = paste(mutationSummary$ANN....GENE, mutationSummary$ANN....HGVS_P, sep = "_"),
+                    t_ref_count = mutationSummary$TUMOR_DP - round(mutationSummary$TUMOR_DP * mutationSummary$TUMOR_MAF),
+                    t_alt_count = round( mutationSummary$TUMOR_DP * mutationSummary$TUMOR_MAF),
+                    dbSNP_Val_Status = rep("validated", nrow(mutationSummary)),
+                    Chromosome = as.numeric(gsub("X", "23", as.character(mutationSummary$CHROM))),
+                    Start_position = mutationSummary$POS)
+
+
+## writes a .txt file per tumor sample
+sapply(sampleList, function(TM) write.mutationSummaryle(absIn[absIn$Tumor_Sample_Barcode == TM , ], file = paste(TM, "muts.txt", sep = "_"), sep = "\t", row.names = FALSE))
+
+save.image(outRData)
