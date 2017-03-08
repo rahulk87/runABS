b1b95c786e66ccb0d5591f2a9deaefc30f99e56f
Rodrigo Gularte Merida <gularter@mskcc.org>
2017-03-08 13:59:26 -0500

    Modified Make_ABS_muts_Input.R to first create an empty data.frame which then is populated with the   information from the mutation summary.

--- Make_ABS_muts_Input.R
+++ Make_ABS_muts_Input.R
@@ -1,27 +1,31 @@
 ## Run to make the mutation input for ABSOLUTE
 
-## setwd('/Users/selenicp/Documents/Projects/LCIS/LCIS Exonseq/')
+## setwd("./")
 
-## You can change this to any file for where you stored the mutation summary sheet. 
+## You can change this to any file for where you stored the mutation summary sheet
 mutationsFile <- "muts.csv" 
 outRData <- "mutations.rda"
 
 mutationSummary <- read.csv(mutationsFile, header = TRUE, stringsAsFactors = FALSE)
 sampleList <- unique(mutationSummary$TUMOR_SAMPLE)
 
-## create a data.frame containing the colums required to run ABSOLUTE
+## create an empty data.frame containing the number of colums required to run ABSOLUTE
 ## columns are in the same order as the `cbind.data.frame` previously used
 ## Tumor_Sample_Barcode, Hugo_Symbol, t_ref_count, t_alt_count, dbSNP_Val_Status, Chromosome, Start_position
-absIn <- data.frame(Tumor_Sample_Barcode = as.character(mutationSummary$TUMOR_SAMPLE),
-                    Hugo_Symbol = paste(mutationSummary$ANN....GENE, mutationSummary$ANN....HGVS_P, sep = "_"),
-                    t_ref_count = mutationSummary$TUMOR_DP - round(mutationSummary$TUMOR_DP * mutationSummary$TUMOR_MAF),
-                    t_alt_count = round( mutationSummary$TUMOR_DP * mutationSummary$TUMOR_MAF),
-                    dbSNP_Val_Status = rep("validated", nrow(mutationSummary)),
-                    Chromosome = as.numeric(gsub("X", "23", as.character(mutationSummary$CHROM))),
-                    Start_position = mutationSummary$POS)
+absIn <- data.frame(matrix(NA, nrow = nrow(mutationSummary), ncol = 7))
+names(absIn) <- c("Tumor_Sample_Barcode", "Hugo_Symbol", "t_ref_count", "t_alt_count", "dbSNP_Val_Status", "Chromosome", "Start_position")
+
+## populate absIn data.frame
+absIn$Tumor_Sample_Barcode <- as.character(mutationSummary$TUMOR_SAMPLE)
+absIn$Hugo_Symbol <- paste(mutationSummary$ANN....GENE, mutationSummary$ANN....HGVS_P, sep = "_")
+absIn$t_alt_count <- round( mutationSummary$TUMOR_DP * mutationSummary$TUMOR_MAF)
+absIn$t_ref_count <- mutationSummary$TUMOR_DP - absIn$t_alt_count
+absIn$dbSNP_Val_Status <- rep("validated", nrow(mutationSummary))
+absIn$Chromosome <- as.numeric(gsub("X", "23", as.character(mutationSummary$CHROM)))
+absIn$Start_position <- mutationSummary$POS)
 
 
-## writes a .txt file per tumor sample
+## loop trhough sampleList and write a .txt file per sample
 sapply(sampleList, function(TM) write.mutationSummaryle(absIn[absIn$Tumor_Sample_Barcode == TM , ], file = paste(TM, "muts.txt", sep = "_"), sep = "\t", row.names = FALSE))
 
 save.image(outRData)
